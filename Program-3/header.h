4cee099179e72d12cfba794634af39ca6b75da53
Alan Labor <stepanove2001@gmail.com>
2026-04-02 00:57:43 +0300

    Added file parsing and choose sorting fields

--- Program-3/header.h
+++ Program-3/header.h
@@ -7,6 +7,8 @@
 #include <limits.h>         // Для константы максимальной длины пути
 #include <iomanip>          // Для вывода информации в табличном виде
 
+#include <wchar.h>
+
 using namespace std;
 
 struct columns; 
@@ -17,10 +19,14 @@ void interface(int choice);     // Отрисовка интерфейса гл�
 void Help();                    // Функция работы пункта меню "Помощь"
 
 void Menu_for_sorting();
+void choose_file_interface();
 int file_parser(columns* array, int SIZE, string file_name);
 int string_counter(string file_name);
 string del_space(string str);
 string inverse_del_space(string str);
 
+int choose_field(columns* array, int SIZE);
+int type_sort();
+void regroup_array(int field, columns* array, int SIZE);
 
 void debug_print(columns* array, int SIZE);          // удалить
--- Program-3/source.cpp
+++ Program-3/source.cpp
@@ -6,7 +6,7 @@ const int FIELDS = 5;               // Количество полей
 struct columns
 {
     string column_name;
-    string* ptr;
+    string* ptr = nullptr;
 };
 
 void Menu(char* argv[])         // Главное меню программы
@@ -30,7 +30,7 @@ void Menu(char* argv[])         // Главное меню программы
         int ch = getch();
         if (ch == 258)
         {
-            if (switcher != 4)
+            if (switcher != 3)
                 switcher += 1;
             else
                 switcher = 1;
@@ -40,7 +40,7 @@ void Menu(char* argv[])         // Главное меню программы
             if (switcher != 1)
                 switcher -= 1;
             else
-                switcher = 4;
+                switcher = 3;
         }
         if (ch == 10)
         {
@@ -50,14 +50,10 @@ void Menu(char* argv[])         // Главное меню программы
             }
             if (switcher == 2)
             {
-                // files_info(&files_ECM_exist, &file_ECM_CONF_exist, argv);
+                Menu_for_sorting();
+                // choose_file_interface();
             }
             if (switcher == 3)
-            {
-                // Menu_for_viewing(&files_ECM_exist, &file_ECM_CONF_exist);
-                Menu_for_sorting();
-            }
-            if (switcher == 4)
             {
                 endwin();
                 system("clear");
@@ -73,16 +69,13 @@ void interface(int choice)      // Отрисовка интерфейса гл�
     switch(choice)
     {
         case 1:
-        printw(" \t\t<< Помощь >>\n \t\tПроверка файлов\n \t\tНачать сортировку\n \t\tВыход");
+        printw(" \t\t<< Помощь >>\n \t\tНачать сортировку\n \t\tВыход");
         break;
         case 2:
-        printw(" \t\tПомощь\n \t\t<< Проверка файлов >>\n \t\tНачать сортировку\n \t\tВыход");
+        printw(" \t\tПомощь\n \t\t<< Начать сортировку >>\n \t\tВыход");
         break;
         case 3:
-        printw(" \t\tПомощь\n \t\tПроверка файлов\n \t\t<< Начать сортировку >>\n \t\tВыход");
-        break;
-        case 4:
-        printw(" \t\tПомощь\n \t\tПроверка файлов\n \t\tНачать сортировку\n \t\t<< Выход >>");
+        printw(" \t\tПомощь\n \t\tНачать сортировку\n \t\t<< Выход >>");
         break;
     }
 }
@@ -119,24 +112,90 @@ void Help()             // Функция работы пункта меню "П
     }
 }
 
+void choose_file_interface()
+{
+    clear();
+    printw("Для возврата в меню нажмите Esc\n");
+    printw("-------------------------------\n\n");
+    printw("Введите название файла: ");
+    refresh();
+    noecho();
+    curs_set(1);            // включение курсора
+    
+    // wchar_t ch;
+    int i=0;
+    wint_t ch;
+    wchar_t filename[256];
+    string str = "";
+
+    // Запоминаем позицию ввода
+    int y, x;
+    getyx(stdscr, y, x);
+
+    while(true)
+    {
+        ch = getch();
+        // get_wch(&ch);     НЕИСПОЛЬЗУЕМ
+        if (ch == 27)
+            break;
+
+        if (ch == KEY_BACKSPACE || ch == 127) { // Backspace
+            // пока без backspace
+            continue;
+            if (!str.empty()) {
+                str.erase(str.length()-1);
+                // if (ch_pred in eng)
+                //     str.erase(str.length()-1);
+                
+                // if (ch_pred in rus)
+                //   str.erase(str.length()-1);
+                // Удаляем последний символ с экрана
+                move(y, x + str.length());
+                delch();
+                delch();
+                refresh();
+            }
+        }
+        else
+        {    addch(ch);
+            str += char(ch);}
+    }
+    endwin();
+    cout << str;
+    curs_set(0);            // выключение курсора
+    exit(0);
+}
+
 void Menu_for_sorting()
 {
-    endwin();           // удалить
+    // string names[FIELDS];       // Инициализируем массив названий полей      ПОКА ОТКАЖЕМСЯ
     // Меню фильтров
+
 
     // парсинг файла
     columns* array = new columns[FIELDS];
-    int flag = file_parser(array, FIELDS, "Отчет_05.03.2026_19-14-19.txt");
-    cout << flag << endl;
-    debug_print(array, FIELDS);
+    int flag = file_parser(array, FIELDS, "Отчет_05.03.2026_19-14-19.txt");         // Считывание полей файла
+    if (flag == -1)
+    {
+        printw("Ошибка чтения файла!");
+        return;
+    }
+    // debug_print(array, FIELDS);
 
-    ///////////
-    exit(0);             // удалить
+    int field = choose_field(array, FIELDS);                // Выбор поля для сортировки
+    if (field == -1)
+        return;
+
+    int type = type_sort();             // выбор типа сортировки (убывание/возрастание)
+    if (type == -1)
+        return;
+
+    regroup_array(field, array, FIELDS);
 }
 
 int file_parser(columns* array, int SIZE, string file_name)         // Возвращает 1, если парсинг файла удался
 {                                                                   // -1 если вознкикла ошибка чтения файла
-    int count = string_counter(file_name);                          // Иначе 0
+    int count = string_counter(file_name);                          
     if (count == -1)
         return -1;
     else
@@ -146,7 +205,7 @@ int file_parser(columns* array, int SIZE, string file_name)         // Возв�
     ifstream file;
     file.open(file_name);
     if (!file.is_open())
-        return 0;
+        return -1;
     else
     {
         string str;
@@ -157,28 +216,23 @@ int file_parser(columns* array, int SIZE, string file_name)         // Возв�
                 int pos = str.find("  ");
                 if ((pos != -1) && (i != SIZE-1))
                 {
-                    string newstr ="";
+                    string newstr = "";
                     string str2 = str;          // Дубликат
                     newstr = str.erase(pos, str.length()-pos);
                     str = str2.erase(0, pos);    
                     str = del_space(str);       // Очистка строки от передних пробелов
                     if (j == 0)
-                        array[i].column_name = newstr;
+                        array[i].column_name = newstr;          // Заполняем название поля
                     else
-                        array[i].ptr[j-1] = newstr;
-
-                    // cout << newstr << "|" << endl;              // Отладочный принт
+                        array[i].ptr[j-1] = newstr;             // Заполняем поле
                 }
                 else if (i == SIZE-1)
                 {   
                     str = inverse_del_space(str);
                     if (j == 0)
-                        array[i].column_name = str;
+                        array[i].column_name = str;             // Заполняем название поля
                     else
-                        array[i].ptr[j-1] = str;
-
-                    // cout << str << "|" << endl;                 // Отладочный принт
-                    // cout << "Строка окончена" << endl;          // Отладочный принт
+                        array[i].ptr[j-1] = str;                // Заполняем поле
                 }
                 else
                 {
@@ -253,5 +307,98 @@ void debug_print(columns* array, int SIZE)
             cout << array[i].ptr[j] << endl;
         }
         cout << endl;
+    }
+}
+
+int choose_field(columns* array, int SIZE)          // Возвращает индекс выбранного элемента массива
+{                                                   // либо возвращает -1 при нажатии Esc
+    int index = 0;
+    while(true)
+    {    
+        clear();
+        printw("Выберите поле, по которому будет выполнена сортировка:\n\n");
+        for (int i=0; i<SIZE; i++)
+        {
+            if (i == index)
+                printw("<< %s >>\n", array[i].column_name.c_str());
+            else 
+                printw(" %s \n", array[i].column_name.c_str());
+        }
+        refresh();
+        int ch = getch();
+        if (ch == 258)
+        {
+            if (index != SIZE-1)
+                index += 1;
+            else
+                index = 0;
+        }
+        if (ch == 259)
+        {
+            if (index != 0)
+                index -= 1;
+            else
+                index = SIZE-1;
+        }
+        if (ch == 10)
+            return index;
+
+        if (ch == 27)
+            return -1;
+    }
+}
+
+int type_sort()                // Возвращает 0, если направление по возрастанию
+{                              // 1, если по убыванию
+    int reverse = 0;           // -1, если нажат Esc
+    while(true)
+    {    
+        clear();
+        printw("Выберите направление сортировки:\n\n");
+        string variants[2] = {"По возрастанию", "По убыванию"};
+        for (int i=0; i<2; i++)
+        {
+            if (i == reverse)
+                printw("<< %s >>\n", variants[i].c_str());
+
+            else 
+                printw(" %s \n", variants[i].c_str());
+        }
+        refresh();
+        int ch = getch();
+        if ((ch == 258) || (ch == 259))
+        {
+            if (reverse == 0)
+                reverse = 1;
+            else
+                reverse = 0;
+        }
+        if (ch == 10)
+            return reverse;
+
+        if (ch == 27)
+            return -1;
+        // reset_prog_mode();
+    }
+}
+
+void regroup_array(int field, columns* array, int SIZE)            // Выполняет перестановку массива структур по выбранному полю
+{
+    columns temp;
+    temp.column_name = array[field].column_name;
+    temp.ptr = array[field].ptr;
+
+    for(int i=field; i!=-1; i--)
+    {
+        if (i == 0)
+        {
+            array[i].column_name = temp.column_name;
+            array[i].ptr = temp.ptr;
+        }
+        else
+        {
+            array[i].column_name = array[i-1].column_name;
+            array[i].ptr = array[i-1].ptr;
+        }
     }
 }
